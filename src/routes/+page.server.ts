91327336fa4bd24ff38369fa73ffda82f326b010
Andrew Hill <contact@andrewthomashill.co.uk>
2024-03-20 18:28:32 +0100

    Add image serverless

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,38 +1,5 @@
-import type { PageLoad, PageServerLoad } from './$types';
+import type { PageServerLoad } from './$types';
 import content from '../content/homepage.json';
-import { createCanvas, Image } from 'canvas';
-
-const loadImage = async (src: string) => {
-  return new Promise<Image>((resolve) => {
-    const img = new Image();
-    img.onload = () => resolve(img);
-    img.src = `./static${src}`;
-  });
-}
-
-const createPixellatedImage = async (src: string, pixelSize = 80) => {
-  const image = await loadImage(src);
-  const canvas = createCanvas(image.width, image.height);
-  const ctx = canvas.getContext('2d');
-  if (!ctx) {
-    return '';
-  }
-
-
-  ctx.imageSmoothingEnabled = false;
-
-  // Calculate the reduced resolution sizes
-  const scaledWidth = Math.ceil(canvas.width / pixelSize);
-  const scaledHeight = Math.ceil(canvas.height / pixelSize);
-
-  // Draw the image in reduced resolution
-  ctx.drawImage(image, 0, 0, scaledWidth, scaledHeight);
-
-  // Scale the image back up to original size, causing the pixellation effect
-  ctx.drawImage(canvas, 0, 0, scaledWidth, scaledHeight, 0, 0, canvas.width, canvas.height);
-
-  return canvas.toDataURL();
-};
 
 export const load: PageServerLoad = async () => {
   const projects = await import.meta.glob('../content/projects/*.json');
@@ -44,11 +11,11 @@ export const load: PageServerLoad = async () => {
 
     const prettySlug = `/${slug.replace('project-', '')}`
 
-    //const image = module.isPrivate ? await createPixellatedImage(module.image, 300) : module.image;
+    const image = module.isPrivate ? `/image?hash=${btoa(module.image)}` : module.image;
 
     return {
       ...module,
-      image: module.image,
+      image,
       default: undefined,
       slug: prettySlug,
     }
---
+++ src/routes/image/+server.ts
@@ -0,0 +1,48 @@
+import { error } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+import { createCanvas, Image } from 'canvas';
+
+const loadImage = async (src: string) => {
+  return new Promise<Image>((resolve) => {
+    const img = new Image();
+    img.onload = () => resolve(img);
+    img.src = `./static${src}`;
+  });
+}
+
+const createPixellatedImage = async (image: Image, pixelSize = 80) => {
+  const canvas = createCanvas(image.width, image.height);
+  const ctx = canvas.getContext('2d');
+
+  ctx.imageSmoothingEnabled = false;
+
+  // Calculate the reduced resolution sizes
+  const scaledWidth = Math.ceil(canvas.width / pixelSize);
+  const scaledHeight = Math.ceil(canvas.height / pixelSize);
+
+  // Draw the image in reduced resolution
+  ctx.drawImage(image, 0, 0, scaledWidth, scaledHeight);
+
+  // Scale the image back up to original size, causing the pixellation effect
+  ctx.drawImage(canvas, 0, 0, scaledWidth, scaledHeight, 0, 0, canvas.width, canvas.height);
+
+  return canvas.toBuffer('image/png');
+};
+
+export const GET: RequestHandler = async ({ setHeaders, request }) => {
+  const hash = new URL(request.url).searchParams.get('hash');
+  const url = atob(hash || '');
+  if (!url) {
+    return error(400, 'Bad Request');
+  }
+
+  const image = await loadImage(url);
+  const pixellatedImage = await createPixellatedImage(image, 300);
+
+  setHeaders({
+    'Content-Type': 'image/png',
+    'Content-Length': pixellatedImage.length
+  });
+
+  return new Response(pixellatedImage);
+};
